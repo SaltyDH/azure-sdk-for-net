fc4c1d647343e84192e1053e6d2b5a9a9d11059d
Matt Psaltis <matt.psaltis@mysite.com.au>
2016-09-19 10:44:07 +1000

    Adds preliminary support for Sql Management

---
+++ src/ResourceManagement/Sql/Microsoft.Azure.Management.Sql/Customizations/DatabasesOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Sql.Models;
+using Microsoft.Rest.Azure;
+
+namespace Microsoft.Azure.Management.Sql
+{
+    internal partial class DatabasesOperations
+    {
+        public Task<AzureOperationResponse<ImportExportOperationStatusResponse>> GetImportExportStatusWithHttpMessagesAsync(string resourceGroupName, string serverName, string databaseName,
+            Guid operationId, Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/ResourceManagement/Sql/Microsoft.Azure.Management.Sql/Customizations/ServersOperations.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Management.Sql.Models;
+using Microsoft.Rest.Azure;
+
+namespace Microsoft.Azure.Management.Sql
+{
+    internal partial class ServersOperations
+    {
+        public Task<AzureOperationResponse<ImportExportOperationStatusResponse>> GetImportStatusWithHttpMessagesAsync(string resourceGroupName, string serverName, Guid operationId,
+            Dictionary<string, List<string>> customHeaders = null, CancellationToken cancellationToken = new CancellationToken())
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
